a7c3acaecebc4d5270cdb132303119ae7fe8dc7a
SimonDevelope <whwnine@gmail.com>
2021-09-13 21:52:13 +0900

    #2. try to use 'get method'

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ require("dotenv").config();
 const Koa = require("koa");
 const Router = require("koa-router");
 const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("koa-bodyparser");
 const app = new Koa();
 const router = new Router();
 const port = process.env.DB_PORT;
@@ -14,8 +16,23 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
 });
 
-router.get("/", (ctx: any, next: any) => {
-  ctx.body = "home";
+connection.connect((err: any) => {
+  if (err) throw err;
+  console.log("connected");
+});
+
+router.get("/siple", async (ctx: any, next: any) => {
+  const sql = "SELECT * FROM siple";
+  const item = await new Promise((resolve: any, reject: any) => {
+    connection.query(sql, (err: any, result: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+  ctx.body = item;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
